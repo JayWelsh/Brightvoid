0c949948bd842c7bb7acd7e96904681ff38954be
jay <thealchemicalopus@gmail.com>
2019-01-21 21:21:36 +0200

    improved pre-render on artwork page & fixed artwork page graphic positioning

--- client/src/components/ArtworkPage.js
+++ client/src/components/ArtworkPage.js
@@ -53,6 +53,64 @@ class ArtworkPage extends React.Component {
     }, 250)
   }
 
+  renderPlaceholder(classes, isConsideredMobile, disableMouseInteractions, artworkTiltOptions) {
+    if (!isConsideredMobile) {
+      return (
+        <Grid container className={classes.root} spacing={24}>
+          <Grid item xs={false} sm={false} md={1} lg={2} className={"disable-padding"}>
+          </Grid>
+          <Grid item xs={12} sm={12} md={10} lg={8}>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <Grid container>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <div style={{ marginTop: '50px', marginBottom: '50px' }}>
+                    <Tilt className={"Tilt" + disableMouseInteractions} options={artworkTiltOptions} style={{ width: '100%' }} >
+                      <div style={{ height: '500px' }} />
+                      <div className={"Tilt-inner"} style={{ position: 'absolute', height: '100%', width: '100%' }}>
+                        <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translateY(-50%)translateX(-50%)' }}>
+                          <AdaptiveHeader headerClass="rock-salt heavy-text-shadow text-center float-effect" variant="h4" component="h1">
+                            Loading...
+                        </AdaptiveHeader>
+                        </div>
+                      </div>
+                    </Tilt>
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <div className={classes.descriptionArea}>
+                    <Typography className="heavy-text-shadow" gutterBottom variant={"h6"}>
+                      Meaning
+                  </Typography>
+                    <Typography className="heavy-text-shadow" gutterBottom variant={"body1"}>
+                      Loading...
+                  </Typography>
+                  </div>
+                </Grid>
+                <Grid container>
+                  <Grid key={1} style={{ paddingBottom: '15px' }} item xs={12} sm={6} md={6} lg={4}>
+                    <SimpleMediaCard headline={"Loading..."} description={"Loading..."} image={' '} link={'./'} />
+                  </Grid>
+                  <Grid key={2} style={{ paddingBottom: '15px' }} item xs={12} sm={6} md={6} lg={4}>
+                    <SimpleMediaCard headline={"Loading..."} description={"Loading..."} image={' '} link={'./'} />
+                  </Grid>
+                  <Grid key={3} style={{ paddingBottom: '15px' }} item xs={12} sm={6} md={6} lg={4}>
+                    <SimpleMediaCard headline={"Loading..."} description={"Loading..."} image={' '} link={'./'} />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={false} sm={false} md={1} lg={2} className={"disable-padding"}>
+          </Grid>
+        </Grid>
+      )
+    } else {
+      return (<div></div>)
+    }
+  }
+
   render() {
     const { classes, artworkNameSEO } = this.props;
     const {initialRender, isConsideredMobile} = this.state;
@@ -99,14 +157,14 @@ class ArtworkPage extends React.Component {
           }
         >
           {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
+            let artworkTiltOptions = { max: 10, speed: 2000  };
+            if (loading) return this.renderPlaceholder(classes, isConsideredMobile, disableMouseInteractions, artworkTiltOptions);
             if (error) return <p>Error :( ${error}</p>;
             if (data && data.artworks[0]) {
               console.log('data.artworks',data.artworks);
               let artwork = data.artworks[0];
               let imageCollections = artwork.imagecollections;
               let productIDToImages = groupImageCollectionByProductID(imageCollections);
-              let artworkTiltOptions = { max: 10, speed: 2000  };
               console.log('productIDToImages', productIDToImages);
               console.log('artwork.images[0]',artwork.images[0].url);
               return (
@@ -120,18 +178,23 @@ class ArtworkPage extends React.Component {
                     <Grid item xs={12} sm={12} md={12} lg={12}>
                       <Grid container>
                         <Grid item xs={12} sm={12} md={12} lg={12}>
-                          {!isConsideredMobile && <div style={{ paddingTop: '50px', paddingBottom: '50px' }}>
-                            <Tilt className={"Tilt" + disableMouseInteractions} options={artworkTiltOptions} style={{ height: 400, width: '100%', background: `url(${artwork.images[0].url}) no-repeat center` }} >
-                              <div className="Tilt-inner">
-                                <AdaptiveHeader headerClass="Tilt-inner rock-salt heavy-text-shadow text-center float-effect" variant="h4" component="h1">
-                                  {artwork.name}
-                                </AdaptiveHeader>
+                          {!isConsideredMobile && <div style={{ marginTop: '50px', marginBottom: '50px' }}>
+                            <Tilt className={"Tilt" + disableMouseInteractions} options={artworkTiltOptions} style={{ width: '100%'}} >
+                              <div style={{ height: '500px' }}>
+                                <img style={{ maxHeight: '100%', height: 'auto', maxWidth: '100%' }} src={artwork.images[0].url} alt={artwork.name} />
+                              </div>
+                              <div className={"Tilt-inner"} style={{ position: 'absolute', height: '100%', width: '100%' }}>
+                                <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translateY(-50%)translateX(-50%)' }}>
+                                  <AdaptiveHeader headerClass="rock-salt heavy-text-shadow text-center float-effect" variant="h4" component="h1">
+                                    {artwork.name}
+                                  </AdaptiveHeader>
+                                </div>
                               </div>
                             </Tilt>
                           </div>}
                             {isConsideredMobile &&
-                              <div>
-                                <img style={{ width: '100%', height: 'auto' }} src={artwork.images[0].url} />
+                              <div style={{position: 'relative'}}>
+                                <img style={{ maxWidth: '100%', height: 'auto' }} src={artwork.images[0].url} alt={artwork.name} />
                                 <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translateY(-50%)translateX(-50%)'}}>
                                 <AdaptiveHeader headerClass="Tilt-inner rock-salt heavy-text-shadow text-center float-effect" variant="h4" component="h1">
                                   {artwork.name}
@@ -152,8 +215,8 @@ class ArtworkPage extends React.Component {
                         </Grid>
                         <Grid container>
                           {artwork.products.map((item, index) =>
-                            <Grid key={item} style={{paddingBottom: (index === (artwork.products.length - 1)) ? '15px' : '15px'}} item xs={12} sm={6} md={6} lg={4}>
-                              <SimpleMediaCard headline={item.name} description={item.description} image={productIDToImages[item._id] ? productIDToImages[item._id][0].url : ''} link={'./' + item.seo + '/'} />
+                            <Grid key={item} style={{paddingBottom: (index === (artwork.products.length - 1) && isConsideredMobile) ? '0px' : '15px'}} item xs={12} sm={6} md={6} lg={4}>
+                              <SimpleMediaCard headline={item.name} description={item.description} image={productIDToImages[item._id] ? productIDToImages[item._id][0].url : ' '} link={'./' + item.seo + '/'} />
                             </Grid>
                           )}
                         </Grid>
